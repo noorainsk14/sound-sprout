fb895c385d69f02abc54ed279db1f1f363738bb9
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-08-29 23:55:21 +0530

    created testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import FeaturedCources from "@/components/FeaturedCources";
 import HeroSection from "@/components/HeroSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
-
+import MusicSchoolTestimonials from "@/components/MusicSchoolTestimonial"
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeaturedCources/>
       <WhyChooseUs/>
+      <MusicSchoolTestimonials/>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
     extend: {
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       keyframes: {
         spotlight: {
@@ -21,6 +23,11 @@ module.exports = {
           "100%": {
             opacity: "1",
             transform: "translate(-50%, -40%) scale(1.3)",
+          },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
           },
         },
       },
