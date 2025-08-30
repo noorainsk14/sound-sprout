d7c5f575911e8b33aa8bfffb5263dac17e204a21
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-08-30 15:58:50 +0530

    created webinar section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import FeaturedCources from "@/components/FeaturedCources";
 import HeroSection from "@/components/HeroSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import MusicSchoolTestimonials from "@/components/MusicSchoolTestimonial"
+import UpcomingWebinar from "@/components/UpcomingWebinar";
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -9,6 +10,7 @@ export default function Home() {
       <FeaturedCources/>
       <WhyChooseUs/>
       <MusicSchoolTestimonials/>
+      <UpcomingWebinar/>
     </main>
   );
 }
